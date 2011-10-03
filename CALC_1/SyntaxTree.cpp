e6fc54a93e5d3a5b40bda6c2ab559a3fe189c7bd
alstein <alstein@727c5742-49f4-4a55-afad-4c9bf3d1b895>
2011-10-03 08:46:28 +0000

    Renamed NodeCreator interface to INodeCreator Added function for constructing XML tree of the expression

--- CALC_1/SyntaxTree.cpp
+++ CALC_1/SyntaxTree.cpp
@@ -26,6 +26,7 @@ enum NodeType {
 	N_TERM1, N_TERM2, N_TERM3, N_TERM4, N_TERMN,
 	N_T1, N_T2, N_T3, N_T4, N_NUMBER};
 
+
 class Node {
 private:
 	string _token;
@@ -60,13 +61,13 @@ public:
 // file is generated with genNodes script
 #include "_Nodes.h"
 
-class NodeCreator {
+class INodeCreator {
 	public:
 	virtual Node *create(string token) = 0;
 };
 
 template<typename T>
-class NodeCreatorImpl: public NodeCreator {
+class NodeCreator: public INodeCreator {
 	public:
 	Node *create(string token) {
 		return new T(token);
@@ -89,65 +90,100 @@ class NodeFactoryException: exception {
 		}
 };
 
-class NodeFactory{
+class NodeFactory {
 	private:
-	static map<NodeType, NodeCreator*> _creators;
-	static NodeFactory
-	NodeFactory() {}
+	static map<NodeType, INodeCreator*> _creators;
 public:
 	
+	static Node *create(NodeType type) {
+		return NodeFactory::create(type, "");
+	}
+
 	static Node *create(NodeType type, string token) {
-		map<NodeType, NodeCreator*>::iterator it = _creators.find(type);
+		map<NodeType, INodeCreator*>::iterator it = _creators.find(type);
 		if (it == _creators.end()) {
 			throw NodeFactoryException(LOG_MSG("Requested NodeCreator not found"));
 		}
-		NodeCreator *creator = it->second;
+		INodeCreator *creator = it->second;
 		Node *node = creator->create(token);
 		return node;
 	}
 	
-	static void registerCreator(NodeType type, NodeCreator *creator) {
+	static void registerCreator(NodeType type, INodeCreator *creator) {
 		// put into the map
-		map<NodeType, NodeCreator*>::iterator  it = _creators.find(type);
-		if(it != _creators.end()) {
+		map<NodeType, INodeCreator*>::iterator  it = _creators.find(type);
+		if (it != _creators.end()) {
 			throw NodeFactoryException(LOG_MSG("NodeCreator already exists"));
 		}
-		_creators.insert(pair<NodeType, NodeCreator*>(type, creator));
+		_creators.insert(pair<NodeType, INodeCreator*>(type, creator));
 	}
 };
 
-map<NodeType, NodeCreator*> NodeFactory::_creators;
+map<NodeType, INodeCreator*> NodeFactory::_creators;
+
+string XMLTree(Node *node, int level) {
+	string xml, start, end;
+
+	start = node->XMLStart();
+	end = node->XMLEnd();
+	
+	for (int i = 0; i < level; ++i) {
+		xml += '\t';
+	}
+
+	xml += start;
+	xml += '\n';
+
+	for(Node::node_iterator it = node->begin();
+			it != node->end(); ++it) {
+		Node *child = *it;
+		xml += XMLTree(child, level + 1);
+	}
+
+	for (int i = 0; i < level; ++i) {
+		xml += '\t';
+	}
+	
+	xml += end;
+	
+	if (end.length() != 0) {
+		xml += '\n';
+	}
+
+	return xml;
+}
+
+string XMLTree(Node *node) {
+	return XMLTree(node, 0);
+}
 
 int main(int argc, char *argv[]) {
-	NodeFactory::registerCreator(N_REAL, new NodeCreatorImpl<RealNode>());
-	NodeFactory::registerCreator(N_INTEGER, new NodeCreatorImpl<IntegerNode>());
-	NodeFactory::registerCreator(N_POWER, new NodeCreatorImpl<PowerNode>());
-	NodeFactory::registerCreator(N_MOD, new NodeCreatorImpl<ModNode>());
-	NodeFactory::registerCreator(N_DIVISION, new NodeCreatorImpl<DivisionNode>());
-	NodeFactory::registerCreator(N_MULTIPLICATION, new NodeCreatorImpl<MultiplicationNode>());
-	NodeFactory::registerCreator(N_MINUS, new NodeCreatorImpl<MinusNode>());
-	NodeFactory::registerCreator(N_PLUS, new NodeCreatorImpl<PlusNode>());
-	NodeFactory::registerCreator(N_OPENING_RBRACKET, new NodeCreatorImpl<OpeningRBracketNode>());
-	NodeFactory::registerCreator(N_CLOSING_RBRACKET, new NodeCreatorImpl<ClosingRBracketNode>());
-	NodeFactory::registerCreator(N_TERM1, new NodeCreatorImpl<Term1Node>());
-	NodeFactory::registerCreator(N_TERM2, new NodeCreatorImpl<Term2Node>());
-	NodeFactory::registerCreator(N_TERM3, new NodeCreatorImpl<Term3Node>());
-	NodeFactory::registerCreator(N_TERM4, new NodeCreatorImpl<Term4Node>());
-	NodeFactory::registerCreator(N_TERMN, new NodeCreatorImpl<TermNNode>());
-	NodeFactory::registerCreator(N_T1, new NodeCreatorImpl<T1Node>());
-	NodeFactory::registerCreator(N_T2, new NodeCreatorImpl<T2Node>());
-	NodeFactory::registerCreator(N_T3, new NodeCreatorImpl<T3Node>());
-	NodeFactory::registerCreator(N_T4, new NodeCreatorImpl<T4Node>());
-	NodeFactory::registerCreator(N_NUMBER, new NodeCreatorImpl<NumberNode>());
+	NodeFactory::registerCreator(N_REAL, new NodeCreator<RealNode>());
+	NodeFactory::registerCreator(N_INTEGER, new NodeCreator<IntegerNode>());
+	NodeFactory::registerCreator(N_POWER, new NodeCreator<PowerNode>());
+	NodeFactory::registerCreator(N_MOD, new NodeCreator<ModNode>());
+	NodeFactory::registerCreator(N_DIVISION, new NodeCreator<DivisionNode>());
+	NodeFactory::registerCreator(N_MULTIPLICATION, new NodeCreator<MultiplicationNode>());
+	NodeFactory::registerCreator(N_MINUS, new NodeCreator<MinusNode>());
+	NodeFactory::registerCreator(N_PLUS, new NodeCreator<PlusNode>());
+	NodeFactory::registerCreator(N_OPENING_RBRACKET, new NodeCreator<OpeningRBracketNode>());
+	NodeFactory::registerCreator(N_CLOSING_RBRACKET, new NodeCreator<ClosingRBracketNode>());
+	NodeFactory::registerCreator(N_TERM1, new NodeCreator<Term1Node>());
+	NodeFactory::registerCreator(N_TERM2, new NodeCreator<Term2Node>());
+	NodeFactory::registerCreator(N_TERM3, new NodeCreator<Term3Node>());
+	NodeFactory::registerCreator(N_TERM4, new NodeCreator<Term4Node>());
+	NodeFactory::registerCreator(N_TERMN, new NodeCreator<TermNNode>());
+	NodeFactory::registerCreator(N_T1, new NodeCreator<T1Node>());
+	NodeFactory::registerCreator(N_T2, new NodeCreator<T2Node>());
+	NodeFactory::registerCreator(N_T3, new NodeCreator<T3Node>());
+	NodeFactory::registerCreator(N_T4, new NodeCreator<T4Node>());
+	NodeFactory::registerCreator(N_NUMBER, new NodeCreator<NumberNode>());
 
-	Node *node = NodeFactory::create(N_PLUS, "+");
-	node->addChild(NodeFactory::create(N_NUMBER, ""));
-	node->addChild(NodeFactory::create(N_INTEGER, "1"));
+	Node *node = NodeFactory::create(N_TERM1);
+	node->addChild(NodeFactory::create(N_TERM2));
+	node->addChild(NodeFactory::create(N_T1));
 	
-	for(Node::node_iterator it = node->begin();
-		it != node->end(); ++it) {
-			Node *node = *it;
-		cout << node->XMLStart() << "\t" << node->XMLEnd() << endl;
-	}
+	cout << XMLTree(node) << endl;
+
 	return 0;
 }
