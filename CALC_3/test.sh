6a6f499c1ed2c9b7b7e23d4aa68f98d8ace02f6e
alstein <alstein@727c5742-49f4-4a55-afad-4c9bf3d1b895>
2011-12-14 15:08:26 +0000

    Added some testing infrustructure

---
+++ CALC_3/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+
+APP=./main
+
+SUCCESS=0
+FAIL=0
+
+if [ ! -x ${APP} ] ; then
+	echo "${APP} not found";
+	exit 1;
+fi
+
+for i in tests/*.sc ; do
+	echo '========== Running test ' "$i" ==========
+	${APP} "$i"
+	if [ "X$?" = "X0" ] ; then
+		echo "Ok";
+		let SUCCESS=$(($SUCCESS+1))
+	else 
+		echo "Failed";
+		let FAIL=$(($FAIL+1))
+	fi
+done
+
+echo 'Total tests ' $(($SUCCESS + $FAIL))
+echo 'Success ' $SUCCESS
+echo 'Fail ' $FAIL
